143c805c7770a9215fffa527f309cbceb94997ff
Arshita Marwaha <arshitamarwaha@gmail.com>
2021-07-24 00:41:11 +0530

    signup login

--- searchES_Flask_api.py
+++ searchES_Flask_api.py
@@ -5,6 +5,7 @@ import re
 import requests
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import bulk
+from flask_sqlalchemy import SQLAlchemy
 import csv
 import nltk
 import tensorflow as tf
@@ -16,6 +17,9 @@ import nltk
 from flask import Flask , render_template, url_for, flash, redirect
 from flask import jsonify
 from forms import RegistrationForm, LoginForm
+from flask_bcrypt import Bcrypt
+from flask_login import UserMixin
+from flask_login import login_user, LoginManager, current_user, logout_user
 
 #sbert_model = SentenceTransformer('bert-base-nli-mean-tokens')
 
@@ -71,6 +75,31 @@ def sentenceSimilaritybyNN(es, sent):
 
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY']= 'b97000541f6faa63839f00bb2e7d8b42'
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager=LoginManager(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id)) 
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    firstname = db.Column(db.String(50), unique=False, nullable=False)
+    lastname = db.Column(db.String(50), unique=False, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(150), unique=False, nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}','{self.fistname}','{self.password}')"
+
+
+
 app.static_folder = 'static'
 es = connect2ES()
 
@@ -91,21 +120,45 @@ def search(query):
                # "tags":str(hit["_source"]["tags"])}
        # ret.append(dic)
      
-    return render_template('searchResult.html', res=res_kw)
+    return render_template('searchResult.html', res=res_kw['hits']['hits'])
 
 @app.route("/home")
 def home():
     return render_template('index.html')
 
-@app.route("/login")
+@app.route("/login" , methods=['GET','POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email=login_form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, login_form.password.data):
+            login_user(user)
+            flash(f'Login successful', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash(f'Login unsuccessful. Please enter valid credentials.','danger')
     return render_template('login.html', login_form = login_form )
 
-@app.route("/signup")
+@app.route("/signup", methods = ['GET', 'POST'])
 def signup():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     signup_form = RegistrationForm()
+    if signup_form.validate_on_submit():
+        hashed_password= bcrypt.generate_password_hash(signup_form.password.data).decode('utf-8')
+        user = User(username = signup_form.username.data,firstname = signup_form.firstname.data, lastname=signup_form.lastname.data, email = signup_form.email.data, password = hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Account created! Please login', 'success')
+        return redirect(url_for('login'))
     return render_template('signup.html', signup_form = signup_form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.run(debug=True)
